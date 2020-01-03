6aa6475ac9c9e03296d1de5fab0a4bdf85ea85ba
Qoosim <plenty4luv@yahoo.com>
2020-01-03 21:41:30 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,6 +1,12 @@
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 def count_turn(board)
-
+  counter = 0
+  board.each do |count|
+    if count == "X" || count == "O"
+      counter += 1
+    end
+  end
+  counter
 end
 
 def current_player(board)
